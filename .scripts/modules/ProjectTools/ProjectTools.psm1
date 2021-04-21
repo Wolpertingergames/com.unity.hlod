57a10b875837c932db3b7a8256a80b5183455002
Andreas Ackermann <ackermanna@gmx.de>
2021-04-21 17:17:21 +0200

    Move com.unity.hlod to root

--- .scripts/modules/ProjectTools/ProjectTools.psm1
+++
@@ -1,5 +0,0 @@
-. $PSScriptRoot/function-Check-LastExitCode.ps1
-. $PSScriptRoot/function-Get-Configuration.ps1
-
-Export-ModuleMember Check-LastExitCode
-Export-ModuleMember Get-Configuration
--- .scripts/modules/ProjectTools/function-Check-LastExitCode.ps1
+++
@@ -1,9 +0,0 @@
-function Check-LastExitCode {
-    if ($LastExitCode -ne 0) {
-        $msg = @"
-EXE RETURNED EXIT CODE $LastExitCode
-CALLSTACK:$(Get-PSCallStack | Out-String)
-"@
-        throw $msg
-    }
-}
